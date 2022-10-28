25e67a14a91bbb20e02b807fd6a427d1723a9e8f
AndreyBoyarchuk <andreyboyarchuk@gmail.com>
2022-10-28 02:07:14 -0600

    adding requirments

--- testprojectandrey/settings.py
+++ testprojectandrey/settings.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,10 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'polls',
+    'storages'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,16 +121,34 @@ USE_I18N = True
 USE_TZ = True
 
 
+
+
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_URL='/static/'
+MEDIA_URL='/images/'
 
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_ROOT=os.path.join(BASE_DIR,'static/images')
 # Enable WhiteNoise's GZip compression of static assets.
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AWS_ACCESS_KEY_ID =config('AWS_ACCESS_KEY_ID'),
+AWS_SECRET_ACCESS_KEY =config('AWS_SECRET_ACCESS_KEY'),
+AWS_STORAGE_BUCKET_NAME =config('AWS_STORAGE_BUCKET_NAME'),
+AWS_QUERYSTRING_AUTH = False
+AWS_S3_FILE_OVERWRITE =False
+AWS_DEFAULT_ACL=None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
