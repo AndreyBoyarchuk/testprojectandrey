d5de6b5bd5f3439d3b52cd46ebb3f99a83529fca
AndreyBoyarchuk <andreyboyarchuk@gmail.com>
2022-10-28 12:10:14 -0600

    adding requirments

--- testprojectandrey/settings.py
+++ testprojectandrey/settings.py
@@ -154,4 +154,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+MEDIA_URL = '/mediafiles/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+
 django_on_heroku.settings(locals())
