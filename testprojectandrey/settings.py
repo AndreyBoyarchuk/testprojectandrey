b6cce85e660352656ebb318baba6d23e42f88510
AndreyBoyarchuk <andreyboyarchuk@gmail.com>
2022-10-28 16:02:11 -0600

    adding requirments

--- testprojectandrey/settings.py
+++ testprojectandrey/settings.py
@@ -35,13 +35,14 @@ ALLOWED_HOSTS = ['testprojectandrey.herokuapp.com','127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls',
+
     'storages',
 ]
 
